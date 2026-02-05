2342f9344b498fb77f563a8079671c8c35b9376f
Theodor Lindner <lindner.theodor@gmail.com>
2026-02-05 15:31:31 +0100

    empty

--- components/pimoroni_mics6814/components/pimoroni_mics6814/__init__.py
+++
@@ -1,6 +0,0 @@
-# Auto-loaded by ESPHome external_components
-CODEOWNERS = ["@you"]
-
-# Force-build dependencies so headers are available even when only the sensor platform is used.
-# This avoids compilation failures when compiling the component without also defining the switch/button/number platforms in YAML.
-DEPENDENCIES = ["i2c", "sensor", "switch", "button", "number"]
